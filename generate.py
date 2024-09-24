730a4cbad96ac889eaf270c8377b92ac0386906e
Isa Bayramov <isa.bayramov.2903@mail.ru>
2024-09-24 20:44:31 +0300

    Добавил комментарии

--- generate.py
+++ generate.py
@@ -12,12 +12,14 @@ import string
 import sys
 
 
+# DOMAIN NAME
 EMAIL_PROVIDERS = [
     "gmail.com",
     "ya.ru",
     "mail.ru",
 ]
 
+# CRUD-операции
 ACTION_TYPES = [
     "CREATE",
     "READ",
@@ -26,31 +28,31 @@ ACTION_TYPES = [
 ]
 
 
+# Генерация рандомных string длиной char_num
 def random_char(char_num):
-       return ''.join(random.choice(string.ascii_letters) for _ in range(char_num))
+    return ''.join(random.choice(string.ascii_letters) for _ in range(char_num))
 
 
+# Генерация email
 def generate_email():
     return f"{random_char(random.randrange(5, 15))}@{random.choice(EMAIL_PROVIDERS)}"
 
 
-
+# Генерация CSV
 if __name__ == "__main__":
-
-    dirname = sys.argv[1]
-    dt = datetime.datetime.strptime(sys.argv[2], '%Y-%m-%d')
-    days_cnt = int(sys.argv[3])
-    emails_cnt = int(sys.argv[4])
-    emails = [generate_email() for _ in range(emails_cnt)]
-    events_cnt = int(sys.argv[5])
+    dirname = sys.argv[1]  # Имя директории
+    dt = datetime.datetime.strptime(sys.argv[2], '%Y-%m-%d')   # Стартовая дата
+    days_cnt = int(sys.argv[3])  # Количество дней
+    emails_cnt = int(sys.argv[4])  # Количество email
+    emails = [generate_email() for _ in range(emails_cnt)]   # Генерация списка эл. адресов
+    events_cnt = int(sys.argv[5])  # Количесвто ежедневных событий
 
     for i in range(days_cnt):
-        current_dt = dt + datetime.timedelta(days=i)
-        filepath = os.path.join(dirname, f"{current_dt.strftime('%Y-%m-%d')}.csv")
-        with open(filepath, 'w') as out:
-            out.write("\n".join(
+        current_dt = dt + datetime.timedelta(days=i)  # Текущая дата
+        filepath = os.path.join(dirname, f"{current_dt.strftime('%Y-%m-%d')}.csv")  # Путь/создание и имя - текущая дата
+        with open(filepath, 'w') as out:  # Открывает файл для записи
+            out.write("\n".join(  # Запись tmail, action, dt
                 f"{random.choice(emails)},{random.choice(ACTION_TYPES)},{current_dt + datetime.timedelta(seconds=random.randrange(0, 60*60*24))}" 
                 for _ in range(events_cnt)
                 )
             )
-            
